caaddd143fc857be3b33402c124e5869fbd0aedf
skarnov <obi.prog@gmail.com>
2021-10-02 15:33:58 +0600

    For Git

---
+++ index.php
@@ -0,0 +1,366 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Marvelous Speed</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta charset="utf-8">
+        <meta name="keywords" content="" />
+        <link href="assets/css/bootstrap.css" rel="stylesheet" type="text/css" media="all" />
+        <link rel="stylesheet" href="assets/css/style.css" type="text/css" media="all" />
+        <link rel="stylesheet" href="css/owl.carousel.css" type="text/css" media="all">
+        <link href="assets/css/owl.theme.css" rel="stylesheet">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.css" integrity="sha256-uXf0U0UCIqBp2J3S53b28h+fpC9uFcA8f+b/NNmiwVk=" crossorigin="anonymous" />
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/fonts/fontawesome-webfont.eot" integrity="undefined" crossorigin="anonymous" />
+        <link href="//fonts.googleapis.com/css?family=Cinzel:400,700,900" rel="stylesheet">
+        <link href="//fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i" rel="stylesheet">
+        <link href='//fonts.googleapis.com/css?family=Roboto+Condensed:400,700italic,700,400italic,300italic,300' rel='stylesheet' type='text/css'>
+        <script src="assets/js/jquery-1.11.1.min.js"></script>
+        <script src="assets/js/bootstrap.js"></script>
+        <script type="text/javascript">
+            jQuery(document).ready(function ($) {
+                $(".scroll").click(function (event) {
+                    event.preventDefault();
+                    $('html,body').animate({scrollTop: $(this.hash).offset().top}, 1000);
+                });
+            });
+        </script> 
+        <script>
+            $(document).ready(function () {
+                $("#owl-demo").owlCarousel({
+                    autoPlay: 3000, //Set AutoPlay to 3 seconds
+                    autoPlay: true,
+                    items: 4,
+                    itemsDesktop: [640, 5],
+                    itemsDesktopSmall: [414, 4]
+                });
+            });
+        </script>
+        <!--[if lt IE 9]>
+          <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <![endif]-->
+    </head>
+    <body>
+        <script src='//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js'></script><script src="//m.servedby-buysellads.com/monetization.js" type="text/javascript"></script>
+        <script>
+            (function () {
+                if (typeof _bsa !== 'undefined' && _bsa) {
+                    // format, zoneKey, segment:value, options
+                    _bsa.init('flexbar', 'CKYI627U', 'placement:w3layoutscom');
+                }
+            })();
+        </script>
+        <script>
+            (function () {
+                if (typeof _bsa !== 'undefined' && _bsa) {
+                    // format, zoneKey, segment:value, options
+                    _bsa.init('fancybar', 'CKYDL2JN', 'placement:demo');
+                }
+            })();
+        </script>
+        <script>
+            (function () {
+                if (typeof _bsa !== 'undefined' && _bsa) {
+                    // format, zoneKey, segment:value, options
+                    _bsa.init('stickybox', 'CKYI653J', 'placement:w3layoutscom');
+                }
+            })();
+        </script>
+        <script type="text/javascript" src="//services.bilsyndication.com/adv1/?d=353" defer="" async=""></script><script> var vitag = vitag || {};
+            vitag.gdprShowConsentTool = false;
+            vitag.outStreamConfig = {type: "slider", position: "left"};</script>
+
+    <body>
+        <!-- banner -->
+        <div class="banner jarallax">
+            <div class="banner-dot">
+                <div class="header-top">
+                    <div class="container">
+                        <div class="header-top-left">
+                            <p><i class="fa fa-home" aria-hidden="true"></i> Av. Dom Dinis No 68A, Loja - 33, 2765-326 Odevelas, Lisbon</p>
+                        </div>
+                        <div class="header-top-right">
+                            <p><i class="fa fa-phone" aria-hidden="true"></i> 00351920362862</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="header">
+                    <div class="container">
+                        <div class="header-left">
+                            <div class="w3layouts-logo">
+                                <h1>
+                                    <a href="index.php">Marvelous Speed</a>
+                                </h1>
+                            </div>
+                        </div>
+                        <div class="clearfix"> </div>
+                    </div>
+                </div>
+                <div class="w3layouts-banner-slider">
+                    <div class="container">
+                        <div class="slider">
+                            <div class="callbacks_container">
+                                <ul class="rslides callbacks callbacks1" id="slider4">
+                                    <li>
+                                        <div class="agileits-banner-info">
+                                            <h3>Best Car Deals<span>in Town</span></h3>
+                                        </div>
+                                    </li>
+                                    <li>
+                                        <div class="agileits-banner-info">
+                                            <h3>Luxurious Car <span>in Your Budget</span></h3>
+                                        </div>
+                                    </li>
+                                </ul>
+                            </div>
+                            <script src="assets/js/responsiveslides.min.js"></script>
+                            <script>
+// You can also use "$(window).load(function() {"
+            $(function () {
+// Slideshow 4
+                $("#slider4").responsiveSlides({
+                    auto: true,
+                    pager: true,
+                    nav: true,
+                    speed: 500,
+                    namespace: "callbacks",
+                    before: function () {
+                        $('.events').append("<li>before event fired.</li>");
+                    },
+                    after: function () {
+                        $('.events').append("<li>after event fired.</li>");
+                    }
+                });
+
+            });
+                            </script>
+                            <!--banner Slider starts Here-->
+                        </div>
+                    </div>
+                </div>
+                <div class="header-right">
+                    <div class="container">
+                        <div class="top-nav">
+                            <nav class="navbar navbar-default">
+                                <div class="navbar-header">
+                                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                                        <span class="sr-only">Toggle navigation</span>
+                                        <span class="icon-bar"></span>
+                                        <span class="icon-bar"></span>
+                                        <span class="icon-bar"></span>
+                                    </button>
+                                </div>
+                                <!--navbar-header-->
+                                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                                    <ul class="nav navbar-nav navbar-right">
+                                        <li><a href="index.php" class="active">Home</a></li>
+                                        <li><a href="#about">About</a></li>
+                                        <li><a href="contact.html">Contact Us</a></li>
+                                    </ul>	
+                                    <div class="clearfix"> </div>	
+                                </div>
+                            </nav>
+                        </div>
+                        <div class="clearfix"> </div>
+                    </div>
+                </div>
+                <div class="banner-social">
+                    <div class="container">
+                        <div class="agileinfo-social-grids">
+                            <ul>
+                                <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                                <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                                <li><a href="#"><i class="fa fa-rss"></i></a></li>
+                                <li><a href="#"><i class="fa fa-vk"></i></a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- //banner -->
+
+        <h1 class="text-center" style="padding-bottom: 5%">
+            Featured Cars
+        </h1>
+
+        <div class="gallery-grids">
+
+            <ul id="da-thumbs" class="da-thumbs">
+                <li>
+                    <a href="" class=" mask b-link-stripe b-animate-go swipebox" title="">
+                        <a href="peugeot.php"><img src="assets/img/1.webp" alt=""></a>
+                    </a>
+                </li>
+                <li>
+                    <a href="" class=" mask b-link-stripe b-animate-go   swipebox" title="">
+                        <a href="skoda.php"><img src="assets/img/4.webp" alt=""></a>
+                    </a>
+                </li>
+                <li>
+                    <a href="" class=" mask b-link-stripe b-animate-go   swipebox" title="">
+                        <a href="renault.php"><img src="assets/img/2.webp" alt=""></a>
+                    </a>
+                </li>
+                <li style="margin-top: 1.4%">
+                    <a href="" class=" mask b-link-stripe b-animate-go   swipebox" title="">
+                        <a href="laguna.php"><img src="assets/img/3.webp" alt=""></a>
+                    </a>
+                </li>
+
+
+            </ul>
+            <div class="clearfix"> </div>
+
+            <!--swipebox -->	
+            <script src="assets/js/jquery.swipebox.min.js"></script> 
+            <script type="text/javascript">
+            jQuery(function ($) {
+                $(".swipebox").swipebox();
+            });
+            </script>
+            <!--//swipebox Ends -->
+        </div>
+
+
+
+        <!-- welcome -->
+        <div class="welcome" id="about">
+            <div class="container">
+                <div class="w3ls-welcome-grids">
+                    <div class="col-md-4 w3l-welcome-left">
+                        <h2>Welcome</h2>
+                        <h5>Hello! This is Shekh Nadir Shah Ali and I am the owner of this company.</h5>
+
+                    </div>
+                    <div class="col-md-4 w3l-welcome-img">
+
+                    </div>
+                    <div class="col-md-4 w3l-welcome-right">
+                        <h3>History of us</h3>
+                        <h5>Founded on 2017</h5>
+                    </div>
+                    <div class="clearfix"> </div>
+                </div>
+            </div>
+        </div>
+        <!-- //welcome -->
+        <!---728x90--->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        <!-- footer -->
+        <div class="w3-agile-footer">
+            <div class="container">
+                <div class="footer-grids">
+                    <div class="col-md-3 footer-grid">
+                        <div class="footer-grid-heading">
+                            <h4>Address</h4>
+                        </div>
+                        <div class="footer-grid-info">
+                            <p>Av. Dom Dinis No 68A, Loja - 33
+                                <span>2765-326 Odevelas, Lisbon</span>
+                            </p>
+                            <p class="phone">Phone : 00351920362862
+                                <span>Email : <a href="mailto:marvelouspeed@gmail.com">marvelouspeed@gmail.com</a></span>
+                                <span>NIF : 514829338</span>
+                            </p>
+                        </div>
+                    </div>
+                    <div class="col-md-3 footer-grid">
+                        <div class="footer-grid-heading">
+                            <h4>Navigation</h4>
+                        </div>
+                        <div class="footer-grid-info">
+                            <ul>
+                                <li><a href="index.php">Home</a></li>
+                                <li><a href="#about">About</a></li>
+                                <li><a href="#contact">Contact</a></li>
+                            </ul>
+                        </div>
+                    </div>
+
+                    <div class="col-md-3 footer-grid">
+                        <div class="footer-grid-heading">
+                            <h4>Follow</h4>
+                        </div>
+                        <div class="social">
+                            <ul>
+                                <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                                <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                                <li><a href="#"><i class="fa fa-rss"></i></a></li>
+                                <li><a href="#"><i class="fa fa-vk"></i></a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="clearfix"> </div>
+                </div>
+                <div class="agileits-w3layouts-copyright">
+                    <p>© 2018-2019 Marvelous Speed Unip. LDA. All Rights Reserved</p>
+                </div>
+            </div>
+        </div>
+        <!-- //footer -->
+        <script src="assets/js/jarallax.js"></script>
+        <script src="assets/js/SmoothScroll.min.js"></script>
+        <script type="text/javascript">
+            /* init Jarallax */
+            $('.jarallax').jarallax({
+                speed: 0.5,
+                imgWidth: 1366,
+                imgHeight: 768
+            })
+        </script>
+
+        <script src="assets/js/responsiveslides.min.js"></script>
+        <script type="text/javascript" src="assets/js/move-top.js"></script>
+        <script type="text/javascript" src="assets/js/easing.js"></script>
+        <!-- here stars scrolling icon -->
+        <script type="text/javascript">
+            $(document).ready(function () {
+                /*
+                 var defaults = {
+                 containerID: 'toTop', // fading element id
+                 containerHoverID: 'toTopHover', // fading element hover id
+                 scrollSpeed: 1200,
+                 easingType: 'linear' 
+                 };
+                 */
+
+                $().UItoTop({easingType: 'easeOutQuart'});
+
+            });
+        </script>
+        <!-- //here ends scrolling icon -->
+        <script src="assets/js/owl.carousel.js"></script>  
+    </body>	
+</html>
---
+++ renault.php
@@ -0,0 +1,211 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Marvelous Speed</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta charset="utf-8">
+        <meta name="keywords" content="" />
+        <link href="assets/css/bootstrap.css" rel="stylesheet" type="text/css" media="all" />
+        <link rel="stylesheet" href="assets/css/style.css" type="text/css" media="all" />
+        <link rel="stylesheet" href="css/owl.carousel.css" type="text/css" media="all">
+        <link href="assets/css/owl.theme.css" rel="stylesheet">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.css" integrity="sha256-uXf0U0UCIqBp2J3S53b28h+fpC9uFcA8f+b/NNmiwVk=" crossorigin="anonymous" />
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/fonts/fontawesome-webfont.eot" integrity="undefined" crossorigin="anonymous" />
+        <link href="//fonts.googleapis.com/css?family=Cinzel:400,700,900" rel="stylesheet">
+        <link href="//fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i" rel="stylesheet">
+        <link href='//fonts.googleapis.com/css?family=Roboto+Condensed:400,700italic,700,400italic,300italic,300' rel='stylesheet' type='text/css'>
+        <script src="assets/js/jquery-1.11.1.min.js"></script>
+        <script src="assets/js/bootstrap.js"></script>
+        <script type="text/javascript">
+            jQuery(document).ready(function ($) {
+                $(".scroll").click(function (event) {
+                    event.preventDefault();
+                    $('html,body').animate({scrollTop: $(this.hash).offset().top}, 1000);
+                });
+            });
+        </script> 
+        <script>
+            $(document).ready(function () {
+                $("#owl-demo").owlCarousel({
+                    autoPlay: 3000, //Set AutoPlay to 3 seconds
+                    autoPlay: true,
+                    items: 4,
+                    itemsDesktop: [640, 5],
+                    itemsDesktopSmall: [414, 4]
+                });
+            });
+        </script>
+        <!--[if lt IE 9]>
+          <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <![endif]-->
+    </head>
+    <body>
+        <script src='//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js'></script><script src="//m.servedby-buysellads.com/monetization.js" type="text/javascript"></script>
+        <script>
+            (function () {
+                if (typeof _bsa !== 'undefined' && _bsa) {
+                    // format, zoneKey, segment:value, options
+                    _bsa.init('flexbar', 'CKYI627U', 'placement:w3layoutscom');
+                }
+            })();
+        </script>
+        <script>
+            (function () {
+                if (typeof _bsa !== 'undefined' && _bsa) {
+                    // format, zoneKey, segment:value, options
+                    _bsa.init('fancybar', 'CKYDL2JN', 'placement:demo');
+                }
+            })();
+        </script>
+        <script>
+            (function () {
+                if (typeof _bsa !== 'undefined' && _bsa) {
+                    // format, zoneKey, segment:value, options
+                    _bsa.init('stickybox', 'CKYI653J', 'placement:w3layoutscom');
+                }
+            })();
+        </script>
+        <script type="text/javascript" src="//services.bilsyndication.com/adv1/?d=353" defer="" async=""></script><script> var vitag = vitag || {};
+            vitag.gdprShowConsentTool = false;
+            vitag.outStreamConfig = {type: "slider", position: "left"};</script>
+    <body>
+        <!-- banner -->
+        <div class="banner jarallax">
+            <div class="banner-dot">
+                <div class="header-top">
+                    <div class="container">
+                        <div class="header-top-left">
+                            <p><i class="fa fa-home" aria-hidden="true"></i> Av. Dom Dinis No 68A, Loja - 33, 2765-326 Odevelas, Lisbon</p>
+                        </div>
+                        <div class="header-top-right">
+                            <p><i class="fa fa-phone" aria-hidden="true"></i> 00351920362862</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="header">
+                    <div class="container">
+                        <div class="header-left">
+                            <div class="w3layouts-logo">
+                                <h1>
+                                    <a href="index.php">Marvelous Speed</a>
+                                </h1>
+                            </div>
+                        </div>
+                        <div class="clearfix"> </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="gallery-grids">
+            <div class="col-md-7">
+                <img src="assets/img/2.webp" alt="">
+            </div>
+            <div class="col-md-5" style="padding: 5%">
+                Anunciante Particular 
+                <ul>
+                    <li>Modelo: Mégane</li>
+                    <li>Condição: Usado</li>
+                    <li>Ano: 2017</li> 
+                    <li>Quilómetros: 100.000</li> 
+                    <li>Matrícula: February 2017</li> 
+                    <li>Portas: 4-5</li> 
+                    <li>Combustível: Diesel</li>
+                </ul>
+                <h4>Price: 16.800 &euro;</h4>
+                <p>For Details Please <a href="mailto:marvelouspeed@gmail.com">Email</a> Us</p>
+            </div>
+            <div class="clearfix"></div>
+            <!--swipebox -->	
+            <script src="assets/js/jquery.swipebox.min.js"></script> 
+            <script type="text/javascript">
+            jQuery(function ($) {
+                $(".swipebox").swipebox();
+            });
+            </script>
+            <!--//swipebox Ends -->
+        </div>
+        <!-- footer -->
+        <div class="w3-agile-footer">
+            <div class="container">
+                <div class="footer-grids">
+                    <div class="col-md-3 footer-grid">
+                        <div class="footer-grid-heading">
+                            <h4>Address</h4>
+                        </div>
+                        <div class="footer-grid-info">
+                            <p>Av. Dom Dinis No 68A, Loja - 33
+                                <span>2765-326 Odevelas, Lisbon</span>
+                            </p>
+                            <p class="phone">Phone : 00351920362862
+                                <span>Email : <a href="mailto:marvelouspeed@gmail.com">marvelouspeed@gmail.com</a></span>
+                                <span>NIF : 514829338</span>
+                            </p>
+                        </div>
+                    </div>
+                    <div class="col-md-3 footer-grid">
+                        <div class="footer-grid-heading">
+                            <h4>Navigation</h4>
+                        </div>
+                        <div class="footer-grid-info">
+                            <ul>
+                                <li><a href="index.php">Home</a></li>
+                                <li><a href="#about">About</a></li>
+                                <li><a href="#contact">Contact</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-md-3 footer-grid">
+                        <div class="footer-grid-heading">
+                            <h4>Follow</h4>
+                        </div>
+                        <div class="social">
+                            <ul>
+                                <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                                <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                                <li><a href="#"><i class="fa fa-rss"></i></a></li>
+                                <li><a href="#"><i class="fa fa-vk"></i></a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="clearfix"> </div>
+                </div>
+                <div class="agileits-w3layouts-copyright">
+                    <p>© 2018-2019 Marvelous Speed Unip. LDA. All Rights Reserved</p>
+                </div>
+            </div>
+        </div>
+        <!-- //footer -->
+        <script src="assets/js/jarallax.js"></script>
+        <script src="assets/js/SmoothScroll.min.js"></script>
+        <script type="text/javascript">
+            /* init Jarallax */
+            $('.jarallax').jarallax({
+                speed: 0.5,
+                imgWidth: 1366,
+                imgHeight: 768
+            })
+        </script>
+
+        <script src="assets/js/responsiveslides.min.js"></script>
+        <script type="text/javascript" src="assets/js/move-top.js"></script>
+        <script type="text/javascript" src="assets/js/easing.js"></script>
+        <!-- here stars scrolling icon -->
+        <script type="text/javascript">
+            $(document).ready(function () {
+                /*
+                 var defaults = {
+                 containerID: 'toTop', // fading element id
+                 containerHoverID: 'toTopHover', // fading element hover id
+                 scrollSpeed: 1200,
+                 easingType: 'linear' 
+                 };
+                 */
+
+                $().UItoTop({easingType: 'easeOutQuart'});
+
+            });
+        </script>
+        <!-- //here ends scrolling icon -->
+        <script src="assets/js/owl.carousel.js"></script>  
+    </body>	
+</html>
